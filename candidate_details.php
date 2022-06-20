1a66416020adaa3747f9c11ccbb9efd153ef6a0f
SidWasTaken <siddharth.shyampur@gmail.com>
2022-06-20 19:20:03 +0530

    candidate insert age fix - return error message

--- candidate_details.php
+++ candidate_details.php
@@ -73,6 +73,13 @@ require_once('check_login.php');
                     <br>
                     <?php
 
+                    if (isset($_GET["e"])) {
+                        echo "<div class='alert alert-warning alert-dismissible fade show' style='font-size: large; text-align:center;'>
+                                <button type='button' class='close' data-dismiss='alert'>&times;</button>
+                                <strong>Warning! </strong>The date entered is invalid.
+                            </div>";
+                    }
+
                     // fetching candidate data from db
                     $result = $conn->query("SELECT * FROM candidate_information");
 
--- candidate_insert.php
+++ candidate_insert.php
@@ -11,15 +11,27 @@ $email = $_POST['email'];
 $wexp = $_POST['workexperience'];
 $qualif = $_POST['qualifications'];
 
-// inserting candidate details
-$res = mysqli_query($conn, "INSERT INTO candidate_information (candidate_fullname, candidate_dob, candidate_age, candidate_gender, candidate_address, phnum, candidate_email, experience_in_years) values ('$fullname', '$dateofbirth', TIMESTAMPDIFF(YEAR, '$dateofbirth', CURDATE()), '$gender', '$address', '$phnumber', '$email', '$wexp')");
+// calculating age from dates
+$today = date("Y-m-d");
+$diff = date_diff(date_create($dateOfBirth), date_create($today));
+$age = $diff->format('%y');
 
-// inserting qualifications of candidate
-$res = mysqli_query($conn, "INSERT INTO candidate_qualifications (candidate_id, qualifications_file_location) values ((select candidate_id from candidate_information order by candidate_id desc limit 1), '$qualif')");
+if($age > 60 || $age < 18)
+{
+    header("location:candidate_details.php?e=1");
+}
 
-// redirects to display candidate information after closing connection
-$conn->close();
-header("location:candidate_details.php");
-exit;
+else
+{
+    // inserting candidate details
+    $res = mysqli_query($conn, "INSERT INTO candidate_information (candidate_fullname, candidate_dob, candidate_age, candidate_gender, candidate_address, phnum, candidate_email, experience_in_years) values ('$fullname', '$dateofbirth', TIMESTAMPDIFF(YEAR, '$dateofbirth', CURDATE()), '$gender', '$address', '$phnumber', '$email', '$wexp')");
 
+    // inserting qualifications of candidate
+    $res = mysqli_query($conn, "INSERT INTO candidate_qualifications (candidate_id, qualifications_file_location) values ((select candidate_id from candidate_information order by candidate_id desc limit 1), '$qualif')");
+
+    // redirects to display candidate information after closing connection
+    $conn->close();
+    header("location:candidate_details.php");
+    exit;
+}
 ?>
